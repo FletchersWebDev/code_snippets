66ba379a61ff0099002bbf72307edf3affe19893
Halewood Artisanal Spirits UK Ltd <76738129+Halewood@users.noreply.github.com>
2021-09-15 09:42:44 +0100

    Update
    Update to include plugin dev snippets and useful websites

---
+++ Wordpress/Plugin Development/includes/functions.php
@@ -0,0 +1,19 @@
+<?php
+
+if (isset($halewood_custom_post_types_options['option_id'])){
+    require_once(plugin_dir_path(__FILE__).'../custom-code.php');
+    require_once(plugin_dir_path(__FILE__).'../custom-code2.php');
+
+    add_filter( 'template_include', 'load_option_template', 99, 1 );
+    function load_option_template($option_template) {
+        global $wp_query, $post;
+            if (is_single() && $post->post_type == ('option')){            
+                $option_template = trailingslashit( plugin_dir_path( __FILE__ ) .'../custom-post-types/options' ).'single-option.php';
+            }
+            
+            else if(!is_single() && $post->post_type == ('option')){
+                $option_template = trailingslashit( plugin_dir_path( __FILE__ ) .'../custom-post-types/options' ).'archive-option.php';    
+            }
+            return $option_template;
+    }
+}
---
+++ Wordpress/Plugin Development/plugin-name.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: My plugin name
+ * Plugin URI: https://repositoryuri.com/plugin-name
+ * Description: My plugin description
+ * Version: 1.0.1
+ * Author: My name
+ * Author URI: https://www.mywebsiteurl.com
+ */
+
+ /**************************************
+ * Includes
+ **************************************/
+require_once('includes/options-page.php');
+require_once('includes/functions.php');
